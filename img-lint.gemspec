f0df22fdd0828c0290185509b55c213c11b00c4e
Anatoli Makarevich <makaroni4@gmail.com>
2018-02-13 23:43:59 +0100

    Drop Filesize dependacy, File#size is enough

--- img-lint.gemspec
+++ img-lint.gemspec
@@ -22,6 +22,4 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.14"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
-
-  spec.add_dependency "filesize", "0.1.1"
 end
--- lib/img_lint/linter.rb
+++ lib/img_lint/linter.rb
@@ -1,21 +1,19 @@
-require "filesize"
-
 module IMGLint
   class Linter
     def lint(config)
-      max_file_size = Filesize.from(config["max_file_size"]).to_i
+      max_file_size = config["max_file_size"].to_i
 
       big_images = Dir.glob(%(#{Dir.pwd}/**/*.{#{config["image_formats"]}})).select do |file|
-        File.size(file) > max_file_size
+        File.new(file).size > max_file_size
       end
 
       if big_images.size > 0
         puts "Suspicious images:"
 
         big_images.each do |image|
-          filesize = Filesize.new("#{File.size(image)} B", Filesize::SI)
+          file_size = File.new(image).size / 1024
 
-          puts [image, filesize.pretty].join("\t")
+          puts [image, file_size].join("\t")
         end
       end
     end
