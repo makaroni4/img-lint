a73ed0174e9c14c8a652e1ba7a335677011ed29d
Anatoli Makarevich <makaroni4@gmail.com>
2018-03-20 19:30:19 +0100

    Add rake integration

---
+++ lib/img_lint/rake_task.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "rake"
+require "rake/tasklib"
+
+module IMGLint
+  # Rake task
+  #
+  class RakeTask < Rake::TaskLib
+    def initialize(name = :img_lint)
+      @name = name
+
+      define
+    end
+
+    private
+
+    def define
+      desc "img-lint"
+
+      task(@name) do |_task, task_args|
+        require "img_lint"
+
+        linter = IMGLint::Linter.new
+        fat_images = linter.lint
+
+        exit fat_images.empty? ? 0 : 2
+      end
+    end
+  end
+end
